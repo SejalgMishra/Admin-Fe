2a6e47101953443e715bd24fff8461e79c235545
SejalgMishra <108781967+SejalgMishra@users.noreply.github.com>
2024-04-29 08:26:19 +0530

    Update AxiosInstance.jsx

--- src/utilities/AxiosInstance.jsx
+++ src/utilities/AxiosInstance.jsx
@@ -1,42 +1,92 @@
-import axios from "axios";
+import axios, { AxiosResponse, ResponseType } from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:3004/api",
-  timeout: "5000",
-  timeoutErrorMessage: "Something went wrong. Try after sometime.",
+import { useAuthStore } from "../store/auth";
+let authToken = useAuthStore.getState().token;
+const removeUser = useAuthStore.getState().removeAll;
+
+useAuthStore.subscribe((state) => {
+  authToken = state.token;
 });
 
-axiosInstance.interceptors.request.use(
+
+axios.interceptors.request.use(
   function (config) {
-    const token = window.localStorage.getItem("token");
-    if (token) {
-      const jsonToken = JSON.parse(token);
-      config.headers.Authorization = `Bearer ${jsonToken.accessToken}`;
+    if (!config.headers) {
+      config.headers;
     }
-    // Do something before request is sent
+    if (authToken) {
+      config.headers["Authorization"] = "Bearer " + authToken;
+    }
+
+    // config.headers["Content-Type"] = "application/json";
+    // config.headers["accept-language"] = 'en';
+    // config.headers["Accept"] = "application/json";
+
     return config;
   },
+
   function (error) {
-    // Do something with request error
     return Promise.reject(error);
   }
 );
 
-// Add a response interceptor
-axiosInstance.interceptors.response.use(
+axios.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response.data;
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    if (error?.response?.data) {
-      return Promise.reject(new Error(error.response.data));
+    if (response.headers["content-type"]?.includes("application/json")) {
+      response = response.data;
+      if (response.status) {
+        return response;
+      } else {
+        return Promise.reject(response);
+      }
+    } else if (response.status === 200) {
+      return response;
     }
-    return Promise.reject(error);
+
+    return Promise.reject(response);
+  },
+
+  function (error) {
+    if (error?.response?.status === 401 || error?.response?.status === 403) {
+      removeUser();
+    }
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    const message =
+      error.response?.status === 404
+        ? "API not found."
+        : error?.response?.data?.message || "Something went wrong";
+    return Promise.reject(message);
   }
 );
 
-export default axiosInstance;
+export const request = async ({
+  url,
+  method = "GET",
+  params,
+  body,
+  headers,
+  responseType,
+}: {
+  url: string;
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  params?: any;
+  body?: any;
+  headers?: any;
+  responseType?: ResponseType;
+}) => {
+  const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL;
+  // const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+
+
+  const res: AxiosResponse = await axios.request({
+    url: BASE_URL + url,
+    method,
+    params,
+    data: body,
+    headers,
+    responseType,
+  });
+
+  return res;
+};
